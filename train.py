ccafca6fb17a6ed460d8fad6b93b69f1e8703329
jaylee <juwonfadairo13@gmail.com>
2026-01-01 08:19:44 +0100

    updated train file

--- train.py
+++ train.py
@@ -7,6 +7,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler, OneHotEncoder, OrdinalEncoder
 from sklearn.impute import SimpleImputer
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 import pickle
 
